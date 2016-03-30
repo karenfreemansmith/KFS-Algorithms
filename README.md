1b522a06662559eee885396237fd508bf602bf0b
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-03-30 11:31:32 -0700

    added form letter

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# jquery
-Getting started with jquery
+# jQuery Practice Pages
+_by Karen Freeman-Smith_
+
+## Various jQuery exercises that were too small for a project of their own.
+
+- make page say what kind of element you click on (index.html)
+- play peek-a-boo with a walrus (peek-a-boo.html)
+- change background colors (colors.html)
+- form letter (letter.html)
+- convert to uppercase (upper.html)
--- js/scripts.js
+++ js/scripts.js
@@ -25,4 +25,34 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("red-background");
   });
+
+  $("#application form").submit(function(event) {
+    var firstnameInput = $("input#firstname").val();
+    var lastnameInput = $("input#lastname").val();
+    var streetInput = $("input#street").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipInput = $("input#zip").val();
+    var companynameInput = $("input#companyname").val();
+    var positionInput = $("input#position").val();
+    var experienceInput = parseInt($("input#experience").val());
+    var dateInput = new Date();
+
+    $(".firstname").text(firstnameInput);
+    $(".lastname").text(lastnameInput);
+    $(".street").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
+    $(".companyname").text(companynameInput);
+    $(".position").text(positionInput);
+    $(".expeienceRequired").text(experienceInput*2);
+    $(".todayDate").text(dateInput.toDateString());
+
+
+    $("#application").hide();
+    $("#letter").show();
+
+    event.preventDefault();
+  });
 });
